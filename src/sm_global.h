8c4ce0a1dff98f50c835958554f7d5d76dccb3d1
reginaldford <631752+reginaldford@users.noreply.github.com>
2023-01-11 02:51:29 -0500

    renamed sm_global_space to sm_global_space_array

--- src/sm_global.h
+++ src/sm_global.h
@@ -5,5 +5,4 @@ sm_memory_heap *sm_global_current_heap(sm_memory_heap *replacement);
 sm_memory_heap *sm_global_other_heap(sm_memory_heap *replacement);
 int             sm_gc_count(int increase);
 double          sm_global_growth_factor(double replacement);
-sm_space_array *sm_global_space(sm_space_array *replacement);
-sm_space_array *sm_global_space_by_pos(sm_space_array *replacement);
+sm_space_array *sm_global_space_array(sm_space_array *replacement);
