1109b1af0ed4af0366298a60b955ef50bd080fde
reginaldford <631752+reginaldford@users.noreply.github.com>
2023-01-11 01:55:37 -0500

    fixed more bugs with space recycling functions

--- src/sm_symbol.c
+++ src/sm_symbol.c
@@ -1,4 +1,5 @@
 // This project is licensed under the BSD 2 clause license. See LICENSE.txt for more information.
+
 #include "sms.h"
 
 sm_symbol *sm_new_symbol(sm_string *sym_name) {
@@ -6,4 +7,8 @@ sm_symbol *sm_new_symbol(sm_string *sym_name) {
   sym->my_type   = sm_symbol_type;
   sym->name      = sym_name;
   return sym;
+}
+
+sm_string *sm_symbol_to_string(sm_symbol *self) {
+  return sm_new_string(self->name->size, &(self->name->content));
 }
