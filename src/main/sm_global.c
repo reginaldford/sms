57e6853f507f79c721cfb2f3fdfd1eac8d9a0b81
Reginald Ford <631752+reginaldford@users.noreply.github.com>
2023-03-02 02:46:29 -0500

    Fixed expression printing regarding new global functions

--- src/main/sm_global.c
+++ src/main/sm_global.c
@@ -92,10 +92,11 @@ char *sm_global_fn_name(unsigned short int which) {
   const unsigned short int num_functions = sm_global_num_fns();
 
   // Should be syncronized with enum sm_expr_type
-  static char *response[] = {"+",    "-",    "*",    "/", "sqrt",     "sin",  "cos", "tan",
-                             "sinh", "cosh", "tanh", "^", "csc",      "sec",  "cot", "ln",
-                             "exp",  "abs",  "diff", "=", "==",       "<",    ">",   "if",
-                             "if",   "let",  "",     "",  "siblings", "prim", "",    "?"};
+  static char *response[] = {"+",    "-",    "*",    "/",        "sqrt", "sin", "cos",
+                             "tan",  "sinh", "cosh", "tanh",     "^",    "csc", "sec",
+                             "cot",  "csch", "sech", "coth",     "ln",   "exp", "abs",
+                             "diff", "=",    "==",   "<",        ">",    "if",  "if",
+                             "let",  "",     "",     "siblings", "prim", "",    "?"};
   if (which >= num_functions) {
     return response[num_functions];
   }
@@ -104,8 +105,8 @@ char *sm_global_fn_name(unsigned short int which) {
 
 // corresponding string length of the string that would come from the sm_global_fn_name(which)
 unsigned int sm_global_fn_name_len(unsigned short int which) {
-  static long unsigned int response_len[] = {1, 1, 1, 1, 4, 3, 3, 3, 4, 4, 4, 1, 3, 3, 3, 2,
-                                             3, 3, 4, 1, 2, 1, 1, 2, 2, 3, 0, 0, 8, 4, 0, 1};
+  static long unsigned int response_len[] = {1, 1, 1, 1, 4, 3, 3, 3, 4, 4, 4, 1, 3, 3, 3, 4, 4, 4,
+                                             2, 3, 3, 4, 1, 2, 1, 1, 2, 2, 3, 0, 0, 8, 4, 0, 1};
   if (which >= sm_global_num_fns()) {
     return 1; // "?"
   }
