3fc048291fc02c04c1761a8963641d1bd7010cc7
631752+reginaldford@users.noreply.github.com <631752+reginaldford@users.noreply.github.com>
2023-04-12 00:40:25 -0400

    Implemented parser error locations. Works for sms -s , sms -i and sms repl mode.

--- src/main/sm_terminal.c
+++ src/main/sm_terminal.c
@@ -1,10 +1,10 @@
 // Read https://raw.githubusercontent.com/reginaldford/sms/main/LICENSE.txt for license information
 
 #include "sms.h"
-
-void sm_terminal_prompt() {
+extern int yylineno;
+void       sm_terminal_prompt() {
   if (!sm_global_options(NULL)->script_flag) {
-    printf("\n> ");
+    printf("\n%i> ", yylineno);
     fflush(stdout);
   }
 }
