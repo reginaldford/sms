d78b91e3a386aca21ee24aa16ecafc02be7a6621
631752+reginaldford@users.noreply.github.com <631752+reginaldford@users.noreply.github.com>
2023-04-12 20:01:46 -0400

    added ability to modify local variables. used this in the new factorial.sms for 'counter'

--- scripts/retromv.sh
+++ scripts/retromv.sh
@@ -1,5 +1,5 @@
 # Use this to let git know that you moved a file after you moved it
-# Use tab completion to find the file, then seperate teh file name from th path
+# Use tab completion to find the file, then seperate the file name from the path
 # Expecting retromv <dstdir> <dstfile>
-mv $1/$2 ./
+mv -v $1/$2 ./
 git mv $2 $1
