ad9ef8f0eb7f7c303cddb9063c2fc4a4822384cd
631752+reginaldford@users.noreply.github.com <631752+reginaldford@users.noreply.github.com>
2023-05-20 12:54:20 -0400

    speed optimizations regarding true/false and sm_strncpy

--- src/main/object/sm_string.c
+++ src/main/object/sm_string.c
@@ -5,7 +5,7 @@
 // Safe string copy
 char *sm_strncpy(char *dest, const char *src, unsigned int n) {
   unsigned int i;
-  for (i = 0; i < n && src[i] != '\0'; i++)
+  for (i = 0; i < n; i++)
     dest[i] = src[i];
   for (; i <= n; i++)
     dest[i] = '\0';
