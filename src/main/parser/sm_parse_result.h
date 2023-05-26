040cfad85e9d78e4a78d46f78b621d2ebc1fe3f7
Reginald Ford <631752+reginaldford@users.noreply.github.com>
2023-05-26 03:30:50 -0400

    Cleaned up parsing functoins and vert programs

--- src/main/parser/sm_parse_result.h
+++ src/main/parser/sm_parse_result.h
@@ -6,7 +6,5 @@ typedef struct sm_parse_result {
 } sm_parse_result;
 
 sm_parse_result sm_parse_stdin();
-sm_parse_result sm_parse_more();
 sm_parse_result sm_parse_cstr(char *cstr, int len);
 sm_parse_result sm_parse_file(char *fname);
-void            sm_parse_done();
