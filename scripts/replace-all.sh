27ad417721ec7efb8a4681c701fddeb09198ba0c
Rex <631752+reginaldford@users.noreply.github.com>
2024-06-29 23:31:37 -0400

    mass replace putln to putLn

--- scripts/replace-all.sh
+++ scripts/replace-all.sh
@@ -1,6 +1,7 @@
 #backup your code before you do this
-str='s/XP_SYMSTR/XP_OPSYM/g';
+str='s/putln/putLn/g';
 find ../src/ -name *.c -exec sed -i $str {} +
 find ../src/ -name *.h -exec sed -i $str {} +
 find ../src/ -name *.y -exec sed -i $str {} +
 find ../src/ -name *.l -exec sed -i $str {} +
+find ../sms_src/examples/ -name *.sms -exec sed -i $str {} +
