dff64557893292633130e9438af12e0e6fc90d8a
Rex <631752+reginaldford@users.noreply.github.com>
2024-08-10 21:58:59 -0400

    more progress on arrays

--- src/main/object/sm_array.c
+++ src/main/object/sm_array.c
@@ -11,6 +11,20 @@ sm_array *sm_new_array(uint16_t type, uint32_t size, sm_object *content, uint32_
   return output;
 }
 
+double sm_f64_array_get_bare(sm_array *a, uint32_t index) {
+  if (index >= a->size || a->inner_type != SM_F64_TYPE) {
+    return 0.0; // or any other appropriate error value
+  }
+  return ((f64 *)&a->content[1])[index];
+}
+
+ui8 sm_ui8_array_get_bare(sm_array *a, uint32_t index) {
+  if (index >= a->size || a->inner_type != SM_UI8_TYPE) {
+    return 0; // or any other appropriate error value
+  }
+  return ((ui8 *)&a->content[1])[index];
+}
+
 // Print to a cstring buffer the description of array
 // Return the resulting length
 uint32_t sm_array_sprint(sm_array *a, char *buffer, bool fake) {
@@ -29,7 +43,7 @@ uint32_t sm_array_sprint(sm_array *a, char *buffer, bool fake) {
     break;
   }
   if (!fake)
-    for (uint8_t i = 0; i < 4; i++)
+    for (uint8_t i = 0; i < len; i++)
       buffer[i] = part1[i];
   len += sm_array_contents_sprint(a, &(buffer[1]), fake);
   if (!fake)
@@ -37,12 +51,39 @@ uint32_t sm_array_sprint(sm_array *a, char *buffer, bool fake) {
   return ++len;
 }
 
-sm_object *sm_array_get_arg(sm_array *a, uint32_t index) {
+sm_object *sm_array_get(sm_array *a, uint32_t index) {
   switch (a->inner_type) {
-  case SM_F64_TYPE:
-    return (sm_object *)(&a->content)[index];
+  case SM_F64_TYPE: {
+    // this number comes out wrong
+    // double num = ((f64 *)&a->content[1])[index];
+    double num = ((f64 *)&(a->content[1]))[index];
+    return (sm_object *)sm_new_f64(num);
+  }
+  case SM_UI8_TYPE: {
+    ui8 num = ((ui8 *)&a->content[1])[index];
+    return (sm_object *)sm_new_ui8(num);
+  }
   }
   return (sm_object *)sms_false;
+}
+
+
+sm_object *sm_f64_array_set(sm_array *a, uint32_t index, sm_f64 *number) {
+  if (index >= a->size)
+    return (sm_object *)sms_false;
+  if (a->inner_type != SM_F64_TYPE)
+    return (sm_object *)sms_false;
+  ((f64 *)&a->content[1])[index] = number->value;
+  return (sm_object *)sms_true;
+}
+
+sm_object *sm_ui8_array_set(sm_array *a, uint32_t index, sm_ui8 *number) {
+  if (index >= a->size)
+    return (sm_object *)sms_false;
+  if (a->inner_type != SM_UI8_TYPE)
+    return (sm_object *)sms_false;
+  ((ui8 *)&a->content[1])[index] = number->value;
+  return (sm_object *)sms_true;
 }
 
 uint32_t sm_array_contents_sprint(sm_array *array, char *buffer, bool fake) {
@@ -51,12 +92,11 @@ uint32_t sm_array_contents_sprint(sm_array *array, char *buffer, bool fake) {
   uint32_t cursor = 0;
   uint32_t i      = 0;
   for (; i + 1 < array->size; i++) {
-    cursor += sm_object_sprint(sm_array_get_arg(array, i), &(buffer[cursor]), fake);
+    cursor += sm_object_sprint(sm_array_get(array, i), &(buffer[cursor]), fake);
     if (!fake)
       buffer[cursor] = ',';
     cursor++;
   }
-  if (array->size > 0)
-    cursor += sm_object_sprint(sm_array_get_arg(array, array->size - 1), &(buffer[cursor]), fake);
+  cursor += sm_object_sprint(sm_array_get(array, array->size - 1), &(buffer[cursor]), fake);
   return cursor;
 }
