f12e3f43399629291fed49b20f2fd0569223c17f
Rex <631752+reginaldford@users.noreply.github.com>
2024-08-13 02:07:00 -0400

    fixed print style for f64 arrays. still working on parsing more than 1 element

--- scripts/replace-all.sh
+++ scripts/replace-all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Backup your code before you do this
-str='s/double/f64/g'
+str='s/17g/16g/g'
 find ../src/ -name "*.c" -exec sed -i -e "$str" {} +
 find ../src/ -name "*.h" -exec sed -i -e "$str" {} +
 find ../src/ -name "*.y" -exec sed -i -e "$str" {} +
