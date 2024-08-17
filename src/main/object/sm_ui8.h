83b1bdd4ed0a1ca257ebe8fb6c4aa32747f54c1e
Rex <631752+reginaldford@users.noreply.github.com>
2024-08-17 06:25:59 -0400

    found gc problem. sm_pointer was larger than sm_ui8. Need a better soln

--- src/main/object/sm_ui8.h
+++ src/main/object/sm_ui8.h
@@ -3,10 +3,12 @@
 /// A boxed ui8-precision floating point value
 typedef struct sm_ui8 {
   uint32_t my_type;
-  uint8_t  value;
-  uint8_t  value2;
-  uint8_t  value3;
-  uint8_t  value4;
+  ui8      value;
+  ui8      filler1;
+  ui8      filler2;
+  ui8      filler3;
+  uint64_t filler4;
+  // temp solution. Might need miniptr
 } sm_ui8;
 
 /// Create a new number
