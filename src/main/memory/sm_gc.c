a8bfe183f2a33a22bc00ba90947bdb4a48554728
rex <rex@red.internal.net>
2024-11-09 12:42:35 -0500

    new deep copy uses existing functions, works for all objects, not used internally yet

--- src/main/memory/sm_gc.c
+++ src/main/memory/sm_gc.c
@@ -16,15 +16,9 @@ sm_object *sm_copy(sm_object *obj) {
 
 // Deep Copy the object
 sm_object *sm_deep_copy(sm_object *obj) {
-  if (obj->my_type == SM_EXPR_TYPE) {
-    sm_expr *expr = (sm_expr *)sm_copy(obj);
-    for (uint32_t i = 0; i < expr->size; i++)
-      sm_expr_set_arg(expr, i, sm_deep_copy(sm_expr_get_arg(expr, i)));
-    return obj;
-  } else {
-    sm_object *new_obj = sm_realloc(obj, sm_sizeof(obj));
-    return new_obj;
-  }
+  sm_object *output = sm_realloc(obj, sm_sizeof(obj));
+  sm_inflate_heap(sms_heap, sms_heap);
+  return output;
 }
 
 // Copy the object to the new heap
