6610553e2f980798b5617be82473ce876d4341b2
631752+reginaldford@users.noreply.github.com <631752+reginaldford@users.noreply.github.com>
2025-02-23 13:15:12 -0500

    changing fib example to use separate output from input

--- clib/fib.c
+++ clib/fib.c
@@ -13,13 +13,25 @@ double fib_recursive(int n) {
     return fib_recursive(n-1)+fib_recursive(n-2);
 }
 
+/*
+ * This is the function that gets loaded by soLoad(...)
+ * The only function signature supported is sm_object * fname(sm_object* input)
+ * The input is a pointer to the sm_expr that is sent from the SMS call.
+ * From here, C code can mutate the values given from SMS.
+ */
 sm_object * fib(sm_object * input){
   sm_expr* array = (sm_expr*)input;
-  sm_object** ptr_array = (sm_object**)(array+1);
-  sm_f64 * n0 = (sm_f64*)(ptr_array[0]);
-  if(n0->my_type == SM_F64_TYPE){
-    n0->value = fib_recursive(n0->value);
+  if(array->size > 2){
+    printf("There should be 2 inputs: the fib sequence index, and a place for the output (a number).\n")
+    return input;
   }
+  sm_object** ptr_array = (sm_object**)(array+1);
+  sm_f64 * input_number = (sm_f64*)(ptr_array[0]);
+  sm_f64 * output = (sm_f64*)(ptr_array[1]);
+  if(input_number->my_type != SM_F64_TYPE || n1 -> my_type != SM_F64_TYPE){
+    printf("Both values given to fib must be floating point numbers.\n");
+  }
+  output->value = fib_recursive(input_number->value);
   return input;
 }
 
