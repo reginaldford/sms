d8fc6b5137dd2f1ffb49ce9c15982916c4d84883
631752+reginaldford@users.noreply.github.com <631752+reginaldford@users.noreply.github.com>
2025-02-23 13:25:35 -0500

    added error handling, fixed benchmark_so.sms

--- clib/fib.c
+++ clib/fib.c
@@ -22,16 +22,17 @@ double fib_recursive(int n) {
 sm_object * fib(sm_object * input){
   sm_expr* array = (sm_expr*)input;
   if(array->size > 2){
-    printf("There should be 2 inputs: the fib sequence index, and a place for the output (a number).\n")
+    printf("There should be 2 inputs: the fib sequence index, and a place for the output_number (a number).\n");
     return input;
   }
   sm_object** ptr_array = (sm_object**)(array+1);
   sm_f64 * input_number = (sm_f64*)(ptr_array[0]);
-  sm_f64 * output = (sm_f64*)(ptr_array[1]);
-  if(input_number->my_type != SM_F64_TYPE || n1 -> my_type != SM_F64_TYPE){
+  sm_f64 * output_number = (sm_f64*)(ptr_array[1]);
+  if(input_number->my_type != SM_F64_TYPE || output_number -> my_type != SM_F64_TYPE){
     printf("Both values given to fib must be floating point numbers.\n");
+    return (sm_object*)input;
   }
-  output->value = fib_recursive(input_number->value);
-  return input;
+  output_number->value = fib_recursive(input_number->value);
+  return (sm_object*)input;
 }
 
