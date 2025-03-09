e8631d3b4304c327322debfa9065ceb734ddf34c
Rex <631752+reginaldford@users.noreply.github.com>
2025-03-09 12:31:14 -0400

    finished get_ffi_type

--- src/main/sm_ffi.c
+++ src/main/sm_ffi.c
@@ -9,19 +9,51 @@ unsigned char foo(unsigned int x, float y) {
   return result;
 }
 
-
-sm_ff_sig *sm_new_ffsignature(sm_expr *input_types, sm_symbol output_type) {
-  struct sm_ff_sig *ffs = (sm_ff_sig *)sm_malloc(sizeof(sm_ff_sig));
-  // Make space for the arg types
-  ffi_type  *arg_types[input_types->size];
-  ffi_status status;
-  ffs->my_type = SM_FF_SIG_TYPE;
-  if ((status = ffi_prep_cif(&ffs->cif, FFI_DEFAULT_ABI, 2, &ffi_type_uint8, arg_types)) !=
-      FFI_OK) {
-    return NULL;
-  }
-  return ffs;
+// Create a new ffi signature object
+struct sm_ff_sig *sm_new_ff_sig(ffi_cif cif) {
+  struct sm_ff_sig *new_ff_sig = sm_malloc(sizeof(struct sm_ff_sig));
+  new_ff_sig->my_type          = SM_FF_SIG_TYPE;
+  new_ff_sig->cif              = cif;
+  return new_ff_sig;
 }
+
+const ffi_type *get_ffi_type(const char *type) {
+  const char *valid_type_strings[] = {
+    "void",           /* ffi_type_void */
+    "pointer",        /* ffi_type_pointer */
+    "uint8",          /* ffi_type_uint8 */
+    "uint16",         /* ffi_type_uint16 */
+    "uint32",         /* ffi_type_uint32 */
+    "uint64",         /* ffi_type_uint64 */
+    "complex_float",  /* ffi_type_complex_float */
+    "complex_double", /* ffi_type_complex_double */
+  };
+
+
+  size_t num_types = sizeof(valid_type_strings) / sizeof(valid_type_strings[0]);
+
+  // Define your array of ffi_type pointers
+
+  /* Array of FFI type pointers */
+  const ffi_type *valid_types[] = {
+    &ffi_type_void,           /* void */
+    &ffi_type_pointer,        /* pointer */
+    &ffi_type_uint8,          /* uint8 (unsigned 8-bit integer) */
+    &ffi_type_uint16,         /* uint16 (unsigned 16-bit integer) */
+    &ffi_type_uint32,         /* uint32 (unsigned 32-bit integer) */
+    &ffi_type_uint64,         /* uint64 (unsigned 64-bit integer) */
+    &ffi_type_complex_float,  /* complex float (complex single-precision floating-point number) */
+    &ffi_type_complex_double, /* complex double (complex double-precision floating-point number) */
+  };
+  // We search through teh string list, return the correlating ffi_type
+  for (size_t i = 0; i < num_types; ++i) {
+    if (strcmp(type, valid_type_strings[i]) == 0) {
+      return valid_types[i];
+    }
+  }
+  return NULL; // Type not found
+}
+
 
 sm_object *sm_ffi_call(sm_ff_fun *fun, sm_object *input) {
   ffi_cif    cif;
@@ -51,13 +83,6 @@ sm_object *sm_ffi_call(sm_ff_fun *fun, sm_object *input) {
   return 0;
 }
 
-// Create a new ffi signature object
-struct sm_ff_sig *sm_new_ff_sig(ffi_cif cif) {
-  struct sm_ff_sig *new_ff_sig = sm_malloc(sizeof(struct sm_ff_sig));
-  new_ff_sig->my_type          = SM_FF_SIG_TYPE;
-  new_ff_sig->cif              = cif;
-  return new_ff_sig;
-}
 
 // If fake is false,
 // Prints to to_str a string describing the ff_sig
