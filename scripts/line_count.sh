d347daa1a6ff638be54f6a1f988ffd22cb77b64d
Rex <631752+reginaldford@users.noreply.github.com>
2025-03-13 23:39:48 -0400

    better line count script

--- scripts/line_count.sh
+++ scripts/line_count.sh
@@ -1,4 +1,6 @@
 # Removes empty lines and counts the lines of code in all .c, .h, .l, and .y files under src
 cd ..
-make clean
+echo "C only:"
+cat src/**/*.c src/**/*.h src/**/**/*.c src/**/**/*.h | grep -E -v '^[[:space:]]*$'  | wc -l
+echo "C, Bison, Flex, and SMS:"
 cat src/**/*.c src/**/*.h src/**/**/*.c src/**/**/*.h src/**/**/*.y src/**/**/*.l sms_src/**/*.sms | grep -E -v '^[[:space:]]*$'  | wc -l
